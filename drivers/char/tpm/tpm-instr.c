7cf192913aff5ffdcb5bdd35e43e0e9d99a9a7be
Adam Ćwiek <adam.cwiek@lpnplant.io>
2021-11-03 14:19:18 +0100

    rapik

---
+++ drivers/char/tpm/tpm-instr.c
@@ -0,0 +1,17 @@
+#include <linux/printk.h>
+
+void __cyg_profile_func_enter(void *this_fn, void *call_site)
+                              __attribute__((no_instrument_function));
+
+void __cyg_profile_func_exit(void *this_fn, void *call_site)
+                             __attribute__((no_instrument_function));
+
+
+void __cyg_profile_func_enter(void *this_fn, void *call_site) {
+  printk("tpm_addr_enter: 0x%px 0x%px \n", this_fn, call_site);
+}
+
+void __cyg_profile_func_exit(void *this_fn, void *call_site) {
+  printk("tpm_addr_exit: 0x%px 0x%px \n", this_fn, call_site);
+}
+
